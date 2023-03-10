d7acc4b6c1e564ca70a4f2a9dd81a4c0d838fd9f
dsymbol <dsymbol@users.noreply.github.com>
2023-03-10 02:01:59 +0200

    rewrite restful booker tests

--- tests/web/test_restful_booker.py
+++ tests/web/test_restful_booker.py
@@ -1,128 +1,105 @@
 import pytest
 
-BASE_URI = "https://automationintesting.online"
-
 
 @pytest.fixture
 def driver(ui_driver):
-    ui_driver.get(BASE_URI)
+    ui_driver.get("https://automationintesting.online")
     ui_driver.wait = 10
     return ui_driver
 
 
-def test_hotel(driver):
-    hotel_img = driver.xpath_element('//img[@class="hotel-logoUrl" or contains(@alt, "logoUrl")]')
-    assert hotel_img.size['height'] > 100 < hotel_img.size['width']
-    assert "png" in hotel_img.get_attribute("src")
-    hotel_description = driver.xpath_element('//div[contains(@class,"hotel-description")]').text
-    assert len(hotel_description) > 10
+def test_admin_panel_login(driver):
+    driver.element('a[href*=admin]').click()
+    driver.wait_for.elements_visibility('#username')
+    driver.element('#username').send_keys('admin')
+    driver.element('#password').send_keys('password')
+    driver.element('#doLogin').click()
+    assert driver.elements('//a[text()="Logout"]')
 
 
-rooms = [(102, "Double", 200, "wifiCheckbox"), (103, "Family", 300, "tvCheckbox"), (104, "Twin", 500, "safeCheckbox")]
+def test_create_room_with_valid_information(driver):
+    test_admin_panel_login(driver)
+
+    name, type_, price, features = 102, "Double", 200, ["Refreshments", "Radio"]
+    driver.element('#roomName').send_keys(name)
+    driver.select_element(driver.element('#accessible')).select_by_value('true')
+    driver.select_element(driver.element('#type')).select_by_value(type_)
+    driver.element('#roomPrice').send_keys(price)
+
+    for feature in features:
+        driver.element(f'input[value={feature}]').click()
+
+    driver.element('#createRoom').click()
+
+    driver.get('https://automationintesting.online')
+
+    result_type = driver.element(f'//img[contains(@alt,"room{name}")] /../.. //h3').text
+    result_features = [i.text for i in driver.elements(f'//img[contains(@alt,"room{name}")] /../.. //li')]
+    assert result_type == type_
+    assert all(i in " ".join(result_features) for i in features)
 
 
-def test_room_data(driver):
-    types = ['single', 'twin', 'double', 'family', 'suite']
-    features = ['wifi', 'refreshments', 'tv', 'safe', 'radio', 'views']
-    # Room type
-    room_type = driver.xpath_elements('//div[contains(@class, "hotel-room-info")] //h3')
-    for rt in room_type:
-        assert rt.text.lower() in types
-    # Room features
-    room_features = driver.xpath_elements('//div[contains(@class, "hotel-room-info")] //ul')
-    room_features = [i.text.lower() for i in room_features][0].split("\n")
-    for rf in room_features:
-        assert rf in features
+def test_create_room_with_incomplete_information(driver):
+    test_admin_panel_login(driver)
+    driver.element('#roomName').send_keys(105)
+    driver.select_element(driver.element('#accessible')).select_by_value('true')
+    driver.select_element(driver.element('#type')).select_by_value('Double')
+    driver.element('#createRoom').click()
+    assert driver.elements('.alert-danger')
 
 
-def test_contact_us(driver, person):
-    driver.xpath_element('//div[contains(@class,"row contact")] //input[@id="name"]').send_keys(
-        person["first_name"])
-    driver.xpath_element('//div[contains(@class,"row contact")] //input[@id="email"]').send_keys(
-        person["email_address"])
-    driver.xpath_element('//div[contains(@class,"row contact")] //input[@id="phone"]').send_keys(
-        person["phone"])
-    driver.xpath_element('//div[contains(@class,"row contact")] //input[@id="subject"]').send_keys(
-        "Test subject")
-    driver.xpath_element('//div[contains(@class,"row contact")] //textarea[@id="description"]').send_keys(
-        "This a test message to validate contact form.")
-    driver.xpath_element('//button[@id="submitContact"]').click()
-    result_name = driver.xpath_element(
-        '//div[contains(@class,"row contact")] //div[@class="col-sm-5"][1] //h2')
-    assert person["first_name"] in result_name.text
-    result_subject = driver.xpath_element(
-        '//div[contains(@class,"row contact")] //div[@class="col-sm-5"][1] //p[@style]')
-    assert "Test subject" in result_subject.text
+def test_cancel_room_booking(driver):
+    test_admin_panel_login(driver)
+    name = 120
+    driver.element('#roomName').send_keys(name)
+    driver.select_element(driver.element('#accessible')).select_by_value('true')
+    driver.select_element(driver.element('#type')).select_by_value('Family')
+    driver.element('#roomPrice').send_keys(500)
+    driver.element('#createRoom').click()
+    driver.element(f'//p[@id="roomName{name}"] /../.. //span[contains(@class, "roomDelete")]').click()
+    assert driver.wait_for.element_invisibility(f'#roomName{name}')
 
 
-def test_admin_panel(driver):
-    driver.xpath_element('//a[contains(@href,"admin")]').click()
-    # Sign in
-    if driver.xpath_element('//input[@id="username"]'):
-        driver.xpath_element('//input[@id="username"]').send_keys("admin")
-        driver.xpath_element('//input[@id="password"]').send_keys("password")
-        driver.xpath_element('//button[@id="doLogin"]').click()
+def test_branding_changes(driver, person):
+    test_admin_panel_login(driver)
+    company = "Homer's Donuts"
+    logo = "https://i.imgur.com/NMJtYCC.jpg"
+    description = "Indulge in delicious donuts from the local bakery, nestled in the hills of Springfield."
+    latitude = 38.905750
+    longitude = 77.032010
 
-    # Create Rooms
-    for room in rooms:
-        driver.xpath_element('//input[@id="roomName"]').send_keys(room[0])
-        type_select = driver.select_element(driver.xpath_element('//select[@id="type"]'))
-        type_select.select_by_value(room[1])
-        driver.xpath_element('//input[@id="roomPrice"]').send_keys(room[2])
-        driver.xpath_element(
-            f'//input[@id="roomPrice"] //parent::div //following-sibling::div //input[@id="{room[3]}"]').click()
-        driver.xpath_element('//button[@id="createRoom"]').click()
+    driver.element('#brandingLink').click()
+    name_field = driver.wait_for.element_clickable('#name')
+    name_field.clear()
+    name_field.send_keys(company)
+    logo_field = driver.element('#logoUrl')
+    logo_field.clear()
+    logo_field.send_keys(logo)
+    description_field = driver.element('#description')
+    description_field.clear()
+    description_field.send_keys(description)
+    latitude_field = driver.element('#latitude')
+    latitude_field.clear()
+    latitude_field.send_keys(latitude)
+    longitude_field = driver.element('#longitude')
+    longitude_field.clear()
+    longitude_field.send_keys(longitude)
+    contactName_field = driver.element('#contactName')
+    contactName_field.clear()
+    contactName_field.send_keys(person.first_name + " " + person.last_name)
+    contactAddress_field = driver.element('#contactAddress')
+    contactAddress_field.clear()
+    contactAddress_field.send_keys(person.address)
+    contactPhone_field = driver.element('#contactPhone')
+    contactPhone_field.clear()
+    contactPhone_field.send_keys(person.phone.replace("-", ""))
+    contactEmail_field = driver.element('#contactEmail')
+    contactEmail_field.clear()
+    contactEmail_field.send_keys(person.email)
 
-    added_rooms = driver.xpath_elements('//div[@data-type="room"] //p[contains(@id,"roomName")]')
-    assert len(added_rooms) > 1
-    for i in added_rooms:
-        assert int(i.text) > 100
+    driver.element('#updateBranding').click()
+    driver.element('#frontPageLink').click()
 
-    # Change branding
-    driver.css_element('*[id=brandingLink]').click()
-
-    new_name = "SeleTest"
-    new_email = "selenium@automation.com"
-    new_phone = 123456789013
-    subject = "Automation testing via Selenium"
-    new_logo = "https://i.imgur.com/JATHGly.png"
-    new_addy = "742 Evergreen Terrace"
-
-    bname = driver.css_element('*[id=name]')
-    bname.clear()
-    bname.send_keys(new_name)
-    blogo = driver.css_element('*[id=logoUrl]')
-    blogo.clear()
-    blogo.send_keys(new_logo)
-    bdescription = driver.css_element('*[id=description]')
-    bdescription.clear()
-    bdescription.send_keys(subject)
-    cname = driver.css_element('*[id=contactName]')
-    cname.clear()
-    cname.send_keys(new_name)
-    caddress = driver.css_element('*[id=contactAddress]')
-    caddress.clear()
-    caddress.send_keys(new_addy)
-    cphone = driver.css_element('*[id=contactPhone]')
-    cphone.clear()
-    cphone.send_keys(new_phone)
-    cemail = driver.css_element('*[id=contactEmail]')
-    cemail.clear()
-    cemail.send_keys(new_email)
-
-    driver.css_element('*[id=updateBranding]').click()
-    if driver.xpath_element('//div[@role="dialog"] //button'):
-        driver.xpath_element('//div[@role="dialog"] //button').click()
-    driver.css_element('*[id=frontPageLink]').click()
-    updated_hotel_img = driver.xpath_element(
-        '//img[@class="hotel-logoUrl" or contains(@alt, "logoUrl")]').get_attribute(
-        "src")
-    # failing appends instead of replacing
-    # assert new_logo == updated_hotel_img
-    updated_description = driver.xpath_element('//div[contains(@class,"hotel-description")] //p').text
-    assert updated_description == subject
-    contact_details = driver.xpath_elements('//div[contains(@class,"row contact")] //p')
-    assert contact_details[0].text == new_name
-    assert contact_details[1].text == new_addy
-    assert int(contact_details[2].text) == new_phone
-    assert contact_details[3].text == new_email
+    driver.wait_for.elements_visibility('img[class=hotel-logoUrl]')
+    assert driver.element('.hotel-description p').text == description
+    assert driver.element('//span[contains(@class, "fa-envelope")] //parent::p').text == person.email
